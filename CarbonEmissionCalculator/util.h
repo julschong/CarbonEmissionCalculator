7743a543b41c1ca9bdb3620132e3f9bfb9495d3a
Julius Lee <7julius.lee@gmail.com>
2021-12-14 10:49:41 -0800

    Add project files.

---
+++ CarbonEmissionCalculator/util.h
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+// This function reads the CSV file. The CSV file contains the zip code, state code, and electricity emission factor for different zip codes
+vector<vector<string>> ReadCSVFile();
